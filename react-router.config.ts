90d862ac5813d0e4778809cef34661d9e90704c2
Tomas Campodonico <tomycsl@gmail.com>
2026-06-16 18:00:30 +0100

    infrastructure: configure automatic deployment workflows matrix via github actions

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  basename: "/charity-cup-2026/",
+  ssr: false,
   future: {
     v8_middleware: true,
     v8_passThroughRequests: true,
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/charity-cup-2026/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
